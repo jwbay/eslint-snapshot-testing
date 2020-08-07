5f465cd927d5bba4027c216c86b7d8d7e2b88d08
Justin Bay <justin.bay@outlook.com>
2020-08-07 16:33:34 -0400

    clean up intermediate validation

--- src/tests/split-fixtures.test.ts
+++ src/tests/split-fixtures.test.ts
@@ -1,28 +1,5 @@
 import { runFixture } from '../ruleSnapshotTester'
 import rule from './rules/camel-case-local-functions'
-import { RuleTester } from 'eslint'
-
-new RuleTester({}).run('wat', rule, {
-	valid: [
-		{
-			code: `
-				function foo() {};
-				foo();`,
-		},
-	],
-	invalid: [
-		{
-			code: `
-			function Foo() {};
-			Foo();`,
-			errors: [
-				{
-					message: 'local function names should be camelCase',
-				},
-			],
-		},
-	],
-})
 
 runFixture({
 	rule,
