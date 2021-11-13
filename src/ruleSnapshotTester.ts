0985043ad2364192d5bfd5437752ae08c6d1b834
Justin Bay <justin.bay@outlook.com>
2021-11-13 17:23:45 -0500

    fix: autofix before/after separation marker alignment in snapshots

--- src/ruleSnapshotTester.ts
+++ src/ruleSnapshotTester.ts
@@ -131,7 +131,7 @@ export function runLintFixtureTests({
 
 			const result = [
 				'Original code:',
-				'========================',
+				'==========================',
 				serializedErrors,
 				'',
 				'Code after applying fixes:',
